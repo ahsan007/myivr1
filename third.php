7b95d56a31f855f2bf4f960a5e9072274312017f
ahsan <iutele7@gmail.com>
2011-06-23 10:07:11 +0530

    third.php

---
+++ third.php
@@ -0,0 +1,14 @@
+<?php
+say("hello this is my first tropo app");
+    ask("What's your four or five digit pin? Press pound when finished", array(
+        "choices"=>"[4-5 DIGITS]",
+        "terminator" => "#",
+        "timeout" => 15.0,
+        "mode" => "dtmf",
+        "onChoice" => "choiceFCN"
+        )
+    );
+    function choiceFCN($event) {
+        say("your pin is $event");
+        }
+?>
