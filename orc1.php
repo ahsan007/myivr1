e8b2470cde5876109eba83315bcf46e405c393e8
ahsan <iutele7@gmail.com>
2011-06-24 15:48:23 +0530

    orc1.php

---
+++ orc1.php
@@ -0,0 +1,9 @@
+<?PHP
+$saconn = mysqli_connect("10.194.111.8","user_11e259c2","$lqgUU%xGi5)c$","db_11e259c2");
+$sql = "SELECT * FROM emp";
+$result = mysqli_query($saconn,$sql);
+while($querry_data=mysqli_fetch_row($result))
+{
+	say("his name is $querry_data[0] and his roll number is $querry_data[1]");
+}
+?>
